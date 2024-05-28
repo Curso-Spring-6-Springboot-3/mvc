79a04dcb4e0541c32eb72a5debc9c8bc7c9de782
David Alfredo Ley Chang <ley.chang@hotmail.com>
2024-05-28 17:42:49 -0600

    Creando un objeto Model

--- src/main/java/com/sparksx/curso/pringboot/webapp/springboot_web_mvc/controllers/UserController.java
+++ src/main/java/com/sparksx/curso/pringboot/webapp/springboot_web_mvc/controllers/UserController.java
@@ -4,14 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.sparksx.curso.pringboot.webapp.springboot_web_mvc.models.User;
+
 @Controller
 public class UserController {
 
     @GetMapping("/details") // Esta es la ruta
     public String details(Model model){
+
+        User user = new User("David", "Ley");
+
         model.addAttribute("title", "Hola mundo Spring Boot");
-        model.addAttribute("name", "Sparksx");
-        model.addAttribute("dev", "fullstack");
+        model.addAttribute("user", user);
 
         return "details";  // Lo que está dentro de "" hace referencia al template que estoy ocupando dentro de src/main/resources/templates
     }
--- src/main/java/com/sparksx/curso/pringboot/webapp/springboot_web_mvc/controllers/UserRestController.java
+++ src/main/java/com/sparksx/curso/pringboot/webapp/springboot_web_mvc/controllers/UserRestController.java
@@ -3,6 +3,9 @@ package com.sparksx.curso.pringboot.webapp.springboot_web_mvc.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sparksx.curso.pringboot.webapp.springboot_web_mvc.models.User;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
@@ -14,11 +17,13 @@ public class UserRestController {
 
     @GetMapping("/details") // Esta es la ruta
     public Map<String, Object> details(){ //No lleva el model porque ya no vamos a regresar una vista si no solo datos
-        Map<String, Object> response = new HashMap<>();
-        response.put("title", "Hola mundo Spring Boot");
-        response.put("name", "Sparksx");
-        response.put("dev", "fullstack");
 
+        User user = new User("David", "Ley");
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("title", "Hola mundo Spring Boot");
+        response.put("user", user);
         return response; 
     }
 }
